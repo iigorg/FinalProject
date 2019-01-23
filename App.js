59dc5a68cc27b35b93178cb3fb7b471e3a651d5f
iigorg <44832390+iigorg@users.noreply.github.com>
2019-01-23 15:35:38 +0200

    redux-persist

--- App.js
+++ App.js
@@ -5,16 +5,31 @@ import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import reducers from './src/reducers'
 import ReduxThunk from 'redux-thunk'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
+//const store = createStore(reducers, applyMiddleware(ReduxThunk))
 
-const store = createStore(reducers, applyMiddleware(ReduxThunk))
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+
+const store = createStore(persistedReducer, applyMiddleware(thunk))
+const persistor = persistStore(store)
 export default class App extends React.Component {
 
   render() {    
    // Expo.ScreenOrientation.allowAsync(Expo.ScreenOrientation.Orientation.PORTRAIT);
     return (
       <Provider store={store}>
-        <MainScreen />
+      <PersistGate loading={null} persistor={persistor}>
+          <MainScreen />
+      </PersistGate>        
       </Provider>
     );
   }
